ba3559981c92b7c1b6ae2846150e530141ab6493
DeepikaSR4 <deepikasanathanan1234@gmail.com>
2023-05-27 20:22:50 +0530

    Added the code

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# quiz-app
+# Quiz App
+<h3>Task 2 of Web development Internship at Sync Intern's</h3>
+<span>May 2023</span>
+
+### Technologies used
+<ul>
+  <li>HTML</li>
+  <li>CSS</li>
+  <li>Bootstrap</li>
+   <li>Javascript</li>
+</ul>
+<br>
+IDE USED : VISUAL STUDIO CODE
+<br>
+
---
+++ script.js
@@ -0,0 +1,95 @@
+
+const quizData = [
+    {
+      question: "Which of the following words is a synonym for 'happy'?",
+      options: ["Sad", "Joyful", "Angry", "Confused"],
+      answer: "Joyful"
+    },
+    {
+      question: "What is the plural form of 'child'?",
+      options: ["Children", "Childs", "Childes", "Childer"],
+      answer: "Children"
+    },
+    {
+      question: "Which of the following is a preposition?",
+      options: ["Jump", "Run", "With", "Sing"],
+      answer: "With"
+    },
+    {
+      question: "What is the comparative form of 'good'?",
+      options: ["Better", "Gooder", "Goodest", "Well"],
+      answer: "Better"
+    },
+    {
+      question: "Which of the following is a conjunction?",
+      options: ["Fast", "And", "Big", "Under"],
+      answer: "And"
+    }
+  ];
+  
+
+  
+  const container = document.querySelector(".container");
+  const questionElem = document.getElementById("question");
+  const optionsElem = document.getElementById("options");
+  const submitBtn = document.getElementById("submit");
+  const scoreElem = document.getElementById("score");
+  
+  let currentQuestion = 0;
+  let score = 0;
+  
+
+  function displayQuestion() {
+    const currentQuiz = quizData[currentQuestion];
+    questionElem.textContent = currentQuiz.question;
+  
+    optionsElem.innerHTML = "";
+    currentQuiz.options.forEach(option => {
+      const radioElem = document.createElement("input");
+      radioElem.type = "radio";
+      radioElem.name = "answer";
+      radioElem.value = option;
+      optionsElem.appendChild(radioElem);
+  
+      const labelElem = document.createElement("label");
+      labelElem.textContent = option;
+      optionsElem.appendChild(labelElem);
+  
+      optionsElem.appendChild(document.createElement("br"));
+    });
+  }
+  
+  // Check selected answer and move to the next question
+  function checkAnswer() {
+    const selectedAnswer = document.querySelector('input[name="answer"]:checked');
+    if (selectedAnswer) {
+      if (selectedAnswer.value === quizData[currentQuestion].answer) {
+        score++;
+      }
+      currentQuestion++;
+  
+      if (currentQuestion < quizData.length) {
+        displayQuestion();
+      } else {
+        let message = "";
+        let emoji = "";
+  
+        if (score === 0 || score === 1) {
+          message = "Oh no! Better luck next time!";
+          emoji = "😞";
+        } else if (score === 2 || score === 3 || score === 4) {
+          message = "Good effort! Keep practicing!";
+          emoji = "👍";
+        } else if (score === 5) {
+          message = "Congratulations! You got a perfect score!";
+          emoji = "🎉";
+        }
+  
+        container.innerHTML = `<h2>Quiz Completed</h2><p>Your score: ${score}/${quizData.length}</p><p>${message}</p><p>${emoji}</p>`;
+      }
+    }
+  }
+  
+  submitBtn.addEventListener("click", checkAnswer);
+  displayQuestion();
+  
